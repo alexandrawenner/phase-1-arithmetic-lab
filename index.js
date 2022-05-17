d1ba40b7ebbd25a1bb9730a0bd2910f418672cc9
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-17 13:27:10 -0400

    commit tree ec34f274677defc73e6f584885d5ec8112bc49ee

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+
+
+const maximum = 10
+const minimum = 1
+
+let random = Math.floor(Math.random() *  (maximum- minimum + 1)) + minimum
+
+
+// Resource https://java2blog.com/generate-random-number-between-1-and-10-in-javascript/?utm_source=rss&utm_medium=rss&utm_campaign=generate-random-number-between-1-and-10-in-javascript#:~:text=We%20can%20simply%20Math.,%2C%20and%201(exclusive).
+
+
+
+let num3 = 16
+let num4 = 6
+ const mod = num3 % num4
+
+
+ let max = Math.max(5, 10, 15, 20)
+console.log(max)
